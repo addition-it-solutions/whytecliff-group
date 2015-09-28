273b6f7b6ae7e3192699d4f806ae7aa05c45d897
Ujjvala-Addsol <rupeshg@aitspl.com>
2015-09-28 19:03:26 +0530

    Create module of Sales Order workflow for Playerlayer DB

---
+++ sales_double_validation/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015-2016 Whytecliff Group Pvt. Ltd.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Sale Order Workflow',
+    'version': '1.0',
+    'author': 'Whytecliff Group Pvt. Ltd.',
+    'category': 'Sales',
+    'summary': 'Double approval on sales order',
+    'website': 'https://www.whytecliffgroup.com',
+    'description': """
+    
+* Double approval on sales order
+
+""",
+    'images': [],
+    'depends': ['sale'],
+    'data': [
+        #'security/ir.model.access.csv',
+        'sales_double_validation_view.xml',
+        #'sales_double_validation_workflow.xml'
+        #'views/payment_form_report.xml',
+        #'whytecliff_payment_report.xml',
+     ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ sales_double_validation/sales_double_validation.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015-2016 Whytecliff Group Pvt. Ltd.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api, _
+
+class playerlayer_sale_order(models.Model):
+    _inherit = "sale.order"
+    
+    state = fields.Selection([
+           ('draft', 'Draft Quotation'),
+           ('sent', 'Quotation Sent'),
+           ('approval1','Approval By School'),
+           ('approval2','Approval By Sales Manager'),
+           ('cancel', 'Cancelled'),
+           ('waiting_date', 'Waiting Schedule'),
+           ('progress', 'Sales Order'),
+           ('manual', 'Sale to Invoice'),
+           ('shipping_except', 'Shipping Exception'),
+           ('invoice_except', 'Invoice Exception'),
+           ('done', 'Done'),
+           ], 'Status', readonly=True, copy=False, help="Gives the status of the quotation or sales order.\
+             \nThe exception status is automatically set when a cancel operation occurs \
+             in the invoice validation (Invoice Exception) or in the picking list process (Shipping Exception).\nThe 'Waiting Schedule' status is set when the invoice is confirmed\
+              but waiting for the scheduler to run on the order date.", select=True)
+              
+    @api.v7
+    def action_button_confirm(self, cr, uid, ids, context=None):
+        #print 'ids-------->',ids
+        #print 'context-------->',context
+        res = super(playerlayer_sale_order,self).action_button_confirm(cr, uid, ids, context=context)
+        #print 'res-------->',res
+        self.write(cr, uid, ids, {'state':'approval2'})
+        return True
+    
+    @api.v7
+    def action_button_confirm_sale(self,cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {'state':'manual'})
+        return True

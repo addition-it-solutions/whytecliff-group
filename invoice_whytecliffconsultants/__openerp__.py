f93baa84e63af13c4ff00c013b9cf18f97fe180d
Ujjvala Gonsalves <ujjvalag@aitspl.com>
2015-11-06 17:57:15 +0530

    [Invoice] Added different invoice layout modules for diff companies.

---
+++ invoice_whytecliffconsultants/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015-2016 Whytecliff Group Pvt. Ltd.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Accounting - Invoice Customizations',
+    'version': '1.0',
+    'author': 'Whytecliff Group Ltd.',
+    'category': 'Accounting & Finance',
+    'summary': 'Customization Invoice Reports',
+    'website': 'https://www.whytecliffgroup.com',
+    'description': """
+Report Customization
+====================
+    * Account Invoice Report
+
+""",
+    'images': [],
+    'depends': ['account'],
+    'data': [
+        'views/report_invoice_document_whytecliffconsultants.xml',
+     ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
